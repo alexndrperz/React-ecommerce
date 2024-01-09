eb5a7feeb41a8e7b56c4a5708717ab26688a5d04
alexndrperz <alan80perez@gmail.com>
2024-01-08 20:18:34 -0400

    feature: Integrating GUI in react

---
+++ gui/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { ProductsCrud } from './views/Admin/ProductsCrud.js';
+import React from 'react'
+
+export const App = () => {
+  return (
+	<>
+		<div>App</div>
+	</>
+
+  )
+}
+
+
+export default App;
