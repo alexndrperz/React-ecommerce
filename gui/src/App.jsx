0f46244b08c1ea2b72907069a6a3f8cb9be27683
alexndrperz <alan80perez@gmail.com>
2024-01-09 19:35:36 -0400

    test: app.jsx modification

--- gui/src/App.jsx
+++ gui/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css';
 import { ProductsCrud } from './views/Admin/ProductsCrud.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react'
 
 export const App = () => {
   return (
-	<>
-		<div>App</div>
-	</>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/poducts' Component={ProductsCrud}/>
+        </Switch>
+      </div>
+    </Router>
+    
 
   )
 }
